c401b685575ce5fddca5ddeee001de9620af7396
preisj <58543036+preisj@users.noreply.github.com>
2023-12-04 09:45:35 -0300

    update

--- front/src/interfaces/CartInterface.tsx
+++
@@ -1,8 +0,0 @@
-export interface CartInterface {
-    id: string;
-    personId: string;
-    bookId: string;
-    quantity: number;
-    createdAt: string;
-    updatedAt: string;
-}
--- front/src/interfaces/OrderItemInterface.tsx
+++
@@ -1,9 +0,0 @@
-export interface OrderItemInterface {
-    id: string;
-    orderId: string;
-    bookId: string;
-    quantity: number;
-    price: number;
-    createdAt: string;
-    updatedAt: string;
-}
